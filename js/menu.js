b97fefc5e980ad9d61ad0b976c63408203129940
Yevhen <eug.z2004@gmail.com>
2022-05-21 16:23:18 +0300

    mobile-menu done

--- js/menu.js
+++ js/menu.js
@@ -2,13 +2,15 @@
   const refs = {
     openModalBtn: document.querySelector("[data-menu-open]"),
     closeModalBtn: document.querySelector("[data-menu-close]"),
-      modal: document.querySelector("[data-menu]"),
+    modal: document.querySelector("[data-menu]"),
+    body: document.querySelector("[data-body]")
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-menu-hidden");
+    refs.modal.classList.toggle("is-hidden");
+    refs.body.classList.toggle("is-hidden")
   }
 })();
